04442e1dcaadbcb987afec7a4e56ac89558ad310
Vicki Chow <dev@vickichowder.com>
2025-03-19 11:14:45 -0400

    Split queue name list by comma

--- src/utils/split-queue-list/split-queue-list.ts
+++ src/utils/split-queue-list/split-queue-list.ts
@@ -1,14 +1,7 @@
-const captureQueuesInList: RegExp = /^(?<queue>[A-Za-z0-9-_]+)$/
-
-const splitQueuesBy: RegExp = /[;\n\s]+?/
+const splitQueuesBy = ',';
 
 export const splitQueueList = (queueList: string): string[] => {
-  const list = queueList.split(splitQueuesBy).filter((name) => {
-    const result = captureQueuesInList.exec(name)
-
-    if (!result?.groups?.queue) return false
-    return result.groups.queue
-  })
+  const list = queueList.split(',')
 
   if (!list.length) throw Error('Queue list must be defined!')
 
