94c96a38ee35db3cac463df44c33edb1ca8e9312
vickichowder <dev@vickichowder.com>
2025-03-19 12:43:52 -0400

    use const

--- src/utils/split-queue-list/split-queue-list.ts
+++ src/utils/split-queue-list/split-queue-list.ts
@@ -1,7 +1,7 @@
 const splitQueuesBy = ',';
 
 export const splitQueueList = (queueList: string): string[] => {
-  const list = queueList.split(',')
+  const list = queueList.split(splitQueuesBy)
 
   if (!list.length) throw Error('Queue list must be defined!')
 
